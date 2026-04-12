9fb4aeffb058a46790f62b5deb46fde5f64b1374
yosue891 <yosueortega630@gmail.com>
2026-04-12 19:59:25 -0400

    Update tools-stickers.js

--- plugins/tools-stickers.js
+++ plugins/tools-stickers.js
@@ -7,32 +7,26 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
         return m.reply(`🌸 ¿Y mi media darling? 💗\nResponde a una imagen o video con\n*${usedPrefix}${command}*`)
     }
 
-    if (!/image|video|webp/.test(mime)) {
-        await m.react('💔')
-        return m.reply('💔 Solo imágenes, videos y gifs, mi amor\~')
-    }
-
     await m.react('🍬')
 
     try {
-        let media = await q.download?.()
-        if (!media) return m.reply('💔 No pude descargar la media, darling...')
+        let media = await q.download()
+        if (!media) throw new Error('No media')
 
         let packname = global.packname || '💗 𝒁𝒆𝒓𝒐 𝑻𝒘𝒐 💗'
         let author = global.author || '© Zore Two'
 
-        await conn.sendMessage(m.chat, { 
-            sticker: media, 
+        await conn.sendImageAsSticker(m.chat, media, m, { 
             packname: packname, 
             author: author 
-        }, { quoted: m })
+        })
         
         await m.react('💗')
 
     } catch (e) {
         console.error(e)
         await m.react('💔')
-        m.reply('💔 Darling, mi sistema falló al crear el sticker... prueba de nuevo~')
+        m.reply('💔 Darling, no pude procesar el sticker. Asegúrate de tener las librerías de stickers instaladas.')
     }
 }
 
