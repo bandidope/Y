4990aba9ec782861851ebc13bc3bfa30814b490b
yosue891 <yosueortega630@gmail.com>
2026-04-12 19:35:33 -0400

    Update tools-stickers.js

--- plugins/tools-stickers.js
+++ plugins/tools-stickers.js
@@ -1,47 +1,38 @@
-import { Sticker, StickerTypes } from 'wa-sticker-formatter'
-import { downloadMediaMessage } from '@whiskeysockets/baileys'
+const handler = async (m, { conn, args, usedPrefix, command }) => {
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || q.mimetype || ''
 
-const handler = async (m, { conn, args, command, usedPrefix }) => {
+    if (!mime) {
+        await m.react('🌸')
+        return m.reply(`🌸 ¿Y mi media darling? 💗\nResponde a una imagen/video con\n*${usedPrefix}${command}*`)
+    }
+
+    if (!/image|video/.test(mime)) {
+        await m.react('💔')
+        return m.reply('💔 Solo imágenes y videos, mi amor\~')
+    }
+
+    await m.react('🍬')
+
     try {
-        let q = m.quoted ? m.quoted : m
-        let mime = (q.msg || q).mimetype || q.mimetype || ''
+        let media = await q.download()
+        let packname = global.packname || '💗 𝒁𝒆𝒓𝒐 𝑻𝒘𝒐 💗'
+        let author = global.author || '© Zore Two'
 
-        if (!mime) {
-            await m.react('🌸')
-            return m.reply(`🌸 ¿Y mi media darling? 💗\nResponde a una imagen/video/gif con\n*${usedPrefix}${command}*`)
-        }
-
-        await m.react('🍬')
-
-        let media = await downloadMediaMessage(q, 'buffer', {}, {
-            reuploadRequest: conn.updateMediaMessage
+        await conn.sendImageAsSticker(m.chat, media, m, {
+            packname: packname,
+            author: author
         })
-
-        if (!media) throw new Error('No se pudo descargar la media')
-
-        let pack = args[0] || global.packname || '💗 𝒁𝒆𝒓𝒐 𝑻𝒘𝒐 💗'
-        let author = args[1] || global.author || '© Zore Two'
-
-        const sticker = new Sticker(media, {
-            pack: pack,
-            author: author,
-            type: StickerTypes.FULL,
-            categories: ['💗'],
-            quality: 60
-        })
-
-        const buffer = await sticker.toBuffer()
-        await conn.sendMessage(m.chat, { sticker: buffer }, { quoted: m })
+        
         await m.react('💗')
-
     } catch (e) {
         console.error(e)
         await m.react('💔')
-        m.reply('💔 Darling, mi sistema no reconoce este comando o la media falló... prueba de nuevo~')
+        m.reply('💔 No pude procesar el sticker, darling...')
     }
 }
 
-handler.help = ['s', 'sticker', 'stiker']
+handler.help = ['s', 'sticker']
 handler.tags = ['stickers']
 handler.command = ['s', 'sticker', 'stiker']
 
